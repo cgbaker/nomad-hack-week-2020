0f79e9715deaffd9519d6aecd468bebf956a748b
Chris Baker <1675087+cgbaker@users.noreply.github.com>
2020-04-15 21:13:44 +0000

    initial version of inkyphat driver

---
+++ epaper-device/device/device.go
@@ -0,0 +1,154 @@
+package device
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	log "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/nomad/plugins/base"
+	"github.com/hashicorp/nomad/plugins/device"
+	"github.com/hashicorp/nomad/plugins/shared/hclspec"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	pluginName = "papirus"
+
+	pluginVersion = "v0.1.0"
+)
+
+var (
+	// pluginInfo provides information used by Nomad to identify the plugin
+	pluginInfo = &base.PluginInfoResponse{
+		Type:              base.PluginTypeDevice,
+		PluginApiVersions: []string{device.ApiVersion010},
+		PluginVersion:     pluginVersion,
+		Name:              pluginName,
+	}
+
+	configSpec = hclspec.NewObject(map[string]*hclspec.Spec{})
+)
+
+type Config struct { }
+
+type DiscoveredInkyPhat struct {
+	width, height int64
+	color string
+}
+
+// InkyPHatPlugin contains a skeleton for most of the implementation of a
+// device plugin.
+type InkyPHatPlugin struct {
+	logger log.Logger
+
+	devices    map[string]*DiscoveredInkyPhat
+	deviceLock sync.RWMutex
+}
+
+// NewPlugin returns a device plugin, used primarily by the main wrapper
+//
+// Plugin configuration isn't available yet, so there will typically be
+// a limit to the initialization that can be performed at this point.
+func NewPlugin(log log.Logger) *InkyPHatPlugin {
+	return &InkyPHatPlugin{
+		logger:  log.Named(pluginName),
+		devices: make(map[string]*DiscoveredInkyPhat),
+	}
+}
+
+// PluginInfo returns information describing the plugin.
+//
+// This is called during Nomad client startup, while discovering and loading
+// plugins.
+func (d *InkyPHatPlugin) PluginInfo() (*base.PluginInfoResponse, error) {
+	return pluginInfo, nil
+}
+
+// ConfigSchema returns the configuration schema for the plugin.
+//
+// This is called during Nomad client startup, immediately before parsing
+// plugin config and calling SetConfig
+func (d *InkyPHatPlugin) ConfigSchema() (*hclspec.Spec, error) {
+	return configSpec, nil
+}
+
+// SetConfig is called by the client to pass the configuration for the plugin.
+func (d *InkyPHatPlugin) SetConfig(c *base.Config) error {
+	return nil
+}
+
+// Fingerprint streams detected devices.
+// Messages should be emitted to the returned channel when there are changes
+// to the devices or their health.
+func (d *InkyPHatPlugin) Fingerprint(ctx context.Context) (<-chan *device.FingerprintResponse, error) {
+	// Fingerprint returns a channel. The recommended way of organizing a plugin
+	// is to pass that into a long-running goroutine and return the channel immediately.
+	outCh := make(chan *device.FingerprintResponse)
+	go d.doFingerprint(ctx, outCh)
+	return outCh, nil
+}
+
+// Stats streams statistics for the detected devices.
+// Messages should be emitted to the returned channel on the specified interval.
+func (d *InkyPHatPlugin) Stats(ctx context.Context, interval time.Duration) (<-chan *device.StatsResponse, error) {
+	outCh := make(chan *device.StatsResponse)
+	return outCh, nil
+}
+
+type reservationError struct {
+	notExistingIDs []string
+}
+
+func (e *reservationError) Error() string {
+	return fmt.Sprintf("unknown device IDs: %s", strings.Join(e.notExistingIDs, ","))
+}
+
+// Reserve returns information to the task driver on on how to mount the given devices.
+// It may also perform any device-specific orchestration necessary to prepare the device
+// for use. This is called in a pre-start hook on the client, before starting the workload.
+func (d *InkyPHatPlugin) Reserve(deviceIDs []string) (*device.ContainerReservation, error) {
+	if len(deviceIDs) == 0 {
+		return &device.ContainerReservation{}, nil
+	}
+
+	// This pattern can be useful for some drivers to avoid a race condition where a device disappears
+	// after being scheduled by the server but before the server gets an update on the fingerprint
+	// channel that the device is no longer available.
+	d.deviceLock.RLock()
+	var notExistingIDs []string
+	for _, id := range deviceIDs {
+		if _, deviceIDExists := d.devices[id]; !deviceIDExists {
+			notExistingIDs = append(notExistingIDs, id)
+		}
+	}
+	d.deviceLock.RUnlock()
+	if len(notExistingIDs) != 0 {
+		return nil, &reservationError{notExistingIDs}
+	}
+
+	// initialize the response
+	resp := &device.ContainerReservation{
+		Envs:    map[string]string{},
+		Mounts:  []*device.Mount{},
+		Devices: []*device.DeviceSpec{},
+	}
+
+	for _, id := range deviceIDs {
+		// Check if the device is known
+		inkyphat, ok := d.devices[id]
+		if !ok {
+			return nil, status.Newf(codes.InvalidArgument, "unknown device %q", id).Err()
+		}
+
+		// Envs are a set of environment variables to set for the task.
+		resp.Envs["INKYPHAT_HEIGHT"] = fmt.Sprint(inkyphat.height)
+		resp.Envs["INKYPHAT_WIDTH"] = fmt.Sprint(inkyphat.width)
+		resp.Envs["INKYPHAT_COLOR"] = inkyphat.color
+	}
+
+	return resp, nil
+}
---
+++ epaper-device/device/fingerprint.go
@@ -0,0 +1,121 @@
+package device
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/nomad/helper/uuid"
+	"github.com/hashicorp/nomad/plugins/device"
+	"github.com/hashicorp/nomad/plugins/shared/structs"
+)
+
+// doFingerprint is the long-running goroutine that detects device changes
+func (d *InkyPHatPlugin) doFingerprint(ctx context.Context, devices chan *device.FingerprintResponse) {
+	defer close(devices)
+
+	// Create a timer that will fire immediately for the first detection
+	ticker := time.NewTimer(0)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			ticker.Reset(30*time.Second)
+			devices <- d.buildFingerprint()
+		}
+	}
+}
+
+var (
+	ID = uuid.Generate()
+)
+
+func (d *InkyPHatPlugin) buildFingerprint() *device.FingerprintResponse {
+
+	d.deviceLock.Lock()
+	defer d.deviceLock.Unlock()
+
+	width, height, color, err := inkyphatStatus()
+	if err != nil {
+		return &device.FingerprintResponse{
+			Error: err,
+		}
+	}
+
+	if width == 0 || height == 0 || color == "" {
+		return nil
+	}
+
+	discovered := &DiscoveredInkyPhat{
+		width:  int64(width),
+		height: int64(height),
+		color:  color,
+	}
+
+	d.devices[ID] = discovered
+
+	deviceGroups := []*device.DeviceGroup{
+		{
+			Vendor:     "pimoroni",
+			Type:       "epaper",
+			Name:       "inkyphat",
+			Devices:    []*device.Device{
+				{
+					ID:         ID,
+					Healthy:    true,
+					HealthDesc: "",
+					HwLocality: nil,
+				},
+			},
+			Attributes: map[string]*structs.Attribute{
+				"width": {Int: &discovered.width},
+				"height": {Int: &discovered.height},
+				"color": {String: &color},
+			},
+		},
+	}
+	return device.NewFingerprint(deviceGroups...)
+}
+
+func inkyphatStatus() (width, height int, color string, err error) {
+	var out bytes.Buffer
+
+	cmd := exec.Command("inkyphat-stats")
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err = cmd.Run()
+	if err != nil {
+		err = fmt.Errorf("failed to determine inkyphat stats: %v", err)
+		return
+	}
+
+	width, height, color = parseInkyPHatStatus(out.String())
+	return
+}
+
+func parseInkyPHatStatus(infoString string) (width, height int, color string) {
+	lines := strings.Split(infoString, "\n")
+	for _, line := range lines {
+		if split := strings.Split(line, ":"); len(split) == 2 {
+			switch key := strings.TrimSpace(split[0]); key {
+			case "width", "height":
+				if i, err := strconv.Atoi(strings.TrimSpace(split[1])); err == nil {
+					if key == "width" {
+						width = i
+					} else {
+						height = i
+					}
+				}
+			case "color":
+				color = strings.TrimSpace(split[2])
+			}
+		}
+	}
+	return
+}
